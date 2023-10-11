9b0185bd0835c346c31dd6d747603a051229a9be
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-10-11 18:56:28 +0300

    comment base

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/logistics/',
+  // base: '/logistics/',
   css: {
     devSourcemap: true,
     preprocessorOptions: {

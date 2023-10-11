47f79960d0a38232bbb3ed2cc44ded46b28108db
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-10-11 20:35:13 +0300

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/maintenance/general/',
+  base: '/maintenance/general',
   css: {
     devSourcemap: true,
     preprocessorOptions: {

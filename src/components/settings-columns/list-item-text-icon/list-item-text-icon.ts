e5c8e667aba223a607883877e8971bc3ed00ddb6
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-10-13 19:39:16 +0300

    update list-item-text-icon

--- src/components/settings-columns/list-item-text-icon/list-item-text-icon.ts
+++ src/components/settings-columns/list-item-text-icon/list-item-text-icon.ts
@@ -1,21 +1,12 @@
 import { defineComponent } from 'vue';
-import type { IMainListItem } from '@/components/settings-columns/interfaces/main-list-item-interface';
-import type { IItemSubmenuList } from '@/views/maintenance-view/interfaces/litem-submenu-list-interface';
 
 export default defineComponent({
   name: 'listItemTextIcon',
-  emits: ['actionClick'],
   props: {
-    items: {
-      type: Array,
-    },
-    keyItem: {
-      type: [String, Number],
+    item: {
+      type: Object,
     },
     icon: {
-      type: String,
-    },
-    customClass: {
       type: String,
     },
   },
@@ -23,11 +14,5 @@ export default defineComponent({
     return {
       sizeIcon: 'xxs',
     };
-  },
-  methods: {
-    // add type for item from list testArray from parrent, which will be in vuex
-    actionClick(item: IMainListItem | IItemSubmenuList): void {
-      this.$emit('actionClick', item);
-    },
   },
 });
